084c3f3ef5997b96829777a236f0ff1bca833ed0
Hamasaqalamidi <rezibarabadze153@gmail.com>
2024-09-25 14:14:43 +0400

    add accordion

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ window.addEventListener('scroll', function() {
 })
 
 
+
+
+
+//accordion
+
+let divArray = document.querySelectorAll('.accordion_container');
+
+for (let item of divArray) {
+  item.addEventListener('click', function() {
+    this.classList.toggle('activeAccordion');
+    
+  })
+}
